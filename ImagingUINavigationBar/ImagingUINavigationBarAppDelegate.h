9b84356d14da3a557e68bafcba8cb92eaac9ce79
Nattapon <totizy@Nattapons-MacBook-Pro.local>
2011-03-19 17:17:20 +0700

    Initial Commit

---
+++ ImagingUINavigationBar/ImagingUINavigationBarAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ImagingUINavigationBarAppDelegate.h
+//  ImagingUINavigationBar
+//
+//  Created by Nattapon on 3/19/11.
+//  Copyright 2011 iphone developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagingUINavigationBarAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ ImagingUINavigationBar/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  ImagingUINavigationBar
+//
+//  Created by Nattapon on 3/19/11.
+//  Copyright 2011 iphone developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
